db914fa25c3a17c9bc63fb6d817eb05dd025655e
mohammad <mfelfelani72@gmail.com>
2024-11-04 15:26:55 +0330

    optimise

--- webpack-files/service-worker/unregister/UnregisterServiceWorker.js
+++ webpack-files/service-worker/unregister/UnregisterServiceWorker.js
@@ -7,7 +7,8 @@ class UnregisterServiceWorker {
         "UnregisterServiceWorker",
         (data, cb) => {
           // const swScriptTag = `<script defer="defer" src="/ScriptRegister.js"></script>`;
-          const swScriptTag = `<script defer="defer">
+          const swScriptTag = 
+          `<script defer="defer">
             window.addEventListener("load", () => {
               navigator.serviceWorker.getRegistrations().then((registrations) => {
                 for (let registration of registrations) {
@@ -15,6 +16,28 @@ class UnregisterServiceWorker {
                 }
               });
             });
+
+            function clearCacheStorage() {
+              if ("caches" in window) {
+                caches
+                  .keys()
+                  .then((cacheNames) => {
+                    cacheNames.forEach((cacheName) => {
+                      caches.delete(cacheName);
+                    });
+                  })
+                  .then(() => {
+                    console.log("All cache storage cleared");
+                  })
+                  .catch((error) => {
+                    console.error("Error clearing cache storage:", error);
+                  });
+              } else {
+                console.log("Cache storage is not supported in this browser");
+              }
+            }
+
+            clearCacheStorage();
           </script>`;
           data.html = data.html.replace(`</body>`, `${swScriptTag}</body>`);
           cb(null, data);
