39dd51328df5531c2b0cdc6a1f3dcbb882f7471a
mohammad <mfelfelani72@gmail.com>
2024-11-05 08:39:53 +0330

    optimise

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,10 @@ module.exports = {
   content: [
     "./public/**/*.html",
     "./src/**/*.{js,jsx,ts,tsx,vue}",
-    flowbite.content(),
+    // flowbite.content(),
   ],
   safelist: [
+    // flowbit
     "w-64",
     "w-1/2",
     "rounded-l-lg",
@@ -24,19 +25,19 @@ module.exports = {
   darkMode: "class",
   theme: {
     fontFamily: {
-       // flowbite
-       sans: ['Graphik', 'sans-serif'],
-       serif: ['Merriweather', 'serif'],
+      // flowbite
+      body: ["Inter", "ui-sans-serif", "system-ui"],
+      sans: ["Inter", "ui-sans-serif", "system-ui"],
     },
     screens: {
       xs: "530px",
-      // => @media (min-width: 475px) { ... }
+      // => @media (min-width: 530px) { ... }
 
       sm: "640px",
       // => @media (min-width: 640px) { ... }
 
       md: "769px",
-      // => @media (min-width: 768px) { ... }
+      // => @media (min-width: 769px) { ... }
 
       bi: "912px",
       // => @media (min-width: 912px) { ... }
@@ -63,14 +64,66 @@ module.exports = {
         black: "#000000",
         red: "#E02D2D",
         white: "#fff",
+      },
+      spacing: {
         // flowbite
-        // gray: colors.coolGray,
-        // blue: colors.lightBlue,
-        // red: colors.rose,
-        // pink: colors.fuchsia,
+        px: "1px",
+        0: "0",
+        0.5: "0.125rem",
+        1: "0.25rem",
+        1.5: "0.375rem",
+        2: "0.5rem",
+        2.5: "0.625rem",
+        3: "0.75rem",
+        3.5: "0.875rem",
+        4: "1rem",
+        5: "1.25rem",
+        6: "1.5rem",
+        7: "1.75rem",
+        8: "2rem",
+        9: "2.25rem",
+        10: "2.5rem",
+        11: "2.75rem",
+        12: "3rem",
+        14: "3.5rem",
+        16: "4rem",
+        20: "5rem",
+        24: "6rem",
+        28: "7rem",
+        32: "8rem",
+        36: "9rem",
+        40: "10rem",
+        44: "11rem",
+        48: "12rem",
+        52: "13rem",
+        56: "14rem",
+        60: "15rem",
+        64: "16rem",
+        72: "18rem",
+        80: "20rem",
+        96: "24rem",
+      },
+      opacity: {
+        // flowbite
+        '0': '0',
+        '20': '0.2',
+        '40': '0.4',
+        '60': '0.6',
+        '80': '0.8',
+        '100': '1',
+      },
+      borderRadius: {
+        // flowbite
+        "4xl": "2rem",
       },
     },
     plugins: [],
   },
-  plugins: [flowbite.plugin()],
+  plugins: [
+    require("flowbite/plugin")({
+      charts: true,
+      forms: true,
+      tooltips: true,
+    }),
+  ],
 };
