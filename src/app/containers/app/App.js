2cb3ff3fd1998bdd24eab67c0f79fe2ba72f1b25
mohammad <mfelfelani72@gmail.com>
2024-11-06 08:34:01 +0330

    optimise

--- src/app/containers/app/App.js
+++ src/app/containers/app/App.js
@@ -1,43 +1,19 @@
 import React, { useEffect, lazy, Suspense } from "react";
-import LoaderPage from "../../../features/core/components/LoaderPage.jsx";
-import i18n from "../../../../utils/services/i18n";
-const Landing = lazy(() => import("../../../features/core/Landing.js"));
 
+// Functions
+import prepareApp from "../../../../utils/lib/prepareApp.js";
+
+// Components
+import LoaderPage from "../../../features/core/components/LoaderPage.jsx";
+const Landing = lazy(() => import("../../../features/core/Landing.js"));
 
 const App = () => {
   useEffect(() => {
-    // { initial language
 
-    const rootHtml = document.getElementById("root-html");
-
-    if (
-      rootHtml &&
-      localStorage.getItem("currentLngId") &&
-      localStorage.getItem("currentLngDir")
-    ) {
-      i18n.changeLanguage(localStorage.getItem("currentLngId"));
-      rootHtml.setAttribute("dir", localStorage.getItem("currentLngDir"));
-    }
-
-    // initial language }
-
-    // { initial theme mode (dark or light)
-
-    if (
-      (window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches) ||
-      localStorage.getItem("theme") === "dark"
-    ) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-
-    // initial theme mode }
+   prepareApp();
+    
   }, []);
 
-
-  
   return (
     <Suspense fallback={<LoaderPage />}>
       <Landing />
---
+++ utils/lib/prepareApp.js
@@ -0,0 +1,36 @@
+import i18n from "../services/i18n";
+
+function prepareApp() {
+    
+  // { initial language
+
+  const rootHtml = document.getElementById("root-html");
+
+  if (
+    rootHtml &&
+    localStorage.getItem("currentLngId") &&
+    localStorage.getItem("currentLngDir")
+  ) {
+    i18n.changeLanguage(localStorage.getItem("currentLngId"));
+    rootHtml.setAttribute("dir", localStorage.getItem("currentLngDir"));
+  }
+
+  // initial language }
+
+  // { initial theme mode (dark or light)
+
+  if (
+    (window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches) ||
+    localStorage.getItem("theme") === "dark"
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
+  // initial theme mode }
+
+}
+
+export default prepareApp
