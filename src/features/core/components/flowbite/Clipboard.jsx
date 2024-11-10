d73616f95d1476eb445be0146ac17ec435546a13
mohammad <mfelfelani72@gmail.com>
2024-11-10 15:57:40 +0330

    edit

--- src/features/core/components/flowbite/Clipboard.jsx
+++ src/features/core/components/flowbite/Clipboard.jsx
@@ -21,7 +21,6 @@ const Clipboard = () => {
 
         <div className="w-full max-w-[16rem]">
           <div className="relative">
-          
             <input
               type="text"
               ref={inputRef}
@@ -46,29 +45,6 @@ const Clipboard = () => {
                   <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
                 </svg>
                 <span className="text-xs font-semibold">{text}</span>
-              </span>
-              <span
-                id="success-message"
-                className="hidden inline-flex items-center"
-              >
-                <svg
-                  className="w-3 h-3 text-blue-700 dark:text-blue-500 me-1.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 16 12"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 5.917 5.724 10.5 15 1.5"
-                  />
-                </svg>
-                <span className="text-xs font-semibold text-blue-700 dark:text-blue-500">
-                  Copied
-                </span>
               </span>
             </button>
           </div>
